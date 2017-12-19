d40f255fddf76f65a72aea49df5915f8fca9e96c
Masato Takahashi <masato.takaha@gmail.com>
2017-12-19 14:57:24 -0800

    added products to recycler adapter

--- app/src/main/java/com/satofromjapan/coderswag/Services/DataService.kt
+++ app/src/main/java/com/satofromjapan/coderswag/Services/DataService.kt
@@ -19,10 +19,18 @@ object DataService {
             Product("Devslopes Graphic Beanie", "$18", "hat1"),
             Product("Devslopes Hat Black", "$20", "hat2"),
             Product("Devslopes Hat White", "$20", "hat3"),
-            Product("Devslopes Hat Snapback", "$22", "hat0")
+            Product("Devslopes Hat Snapback", "$22", "hat4"),
+            Product("Devslopes Graphic Beanie", "$18", "hat1"),
+            Product("Devslopes Hat Black", "$20", "hat2"),
+            Product("Devslopes Hat White", "$20", "hat3"),
+            Product("Devslopes Hat Snapback", "$22", "hat4")
     )
 
     val hoodies = listOf(
+            Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
+            Product("Devslopes Hoodie Red", "$28", "hoodie2"),
+            Product("Devslopes Hoodie White", "$28", "hoodie3"),
+            Product("Devslopes Hoodie Black", "$28", "hoodie4"),
             Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
             Product("Devslopes Hoodie Red", "$28", "hoodie2"),
             Product("Devslopes Hoodie White", "$28", "hoodie3"),
@@ -30,6 +38,11 @@ object DataService {
     )
 
     val shirts = listOf(
+            Product("Devslopes Shirt Black", "$20", "shirt1"),
+            Product("Devslopes Badge Light Gray", "$30", "shirt2"),
+            Product("Devslopes HLogo Shirt Red", "$28", "shirt3"),
+            Product("Devslopes Hustle", "$28", "shirt4"),
+            Product("Kickflip Studios", "$28", "shirt5"),
             Product("Devslopes Shirt Black", "$20", "shirt1"),
             Product("Devslopes Badge Light Gray", "$30", "shirt2"),
             Product("Devslopes HLogo Shirt Red", "$28", "shirt3"),
